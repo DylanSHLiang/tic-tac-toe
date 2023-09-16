c3188ad433a81d8931b9744028418b416ab250c1
Dylan Liang <112071868+DylanSHLiang@users.noreply.github.com>
2023-09-16 21:03:14 +1000

    Added funcionality to play against a bot

--- scripts.js
+++ scripts.js
@@ -1,7 +1,73 @@
 const boxes = document.querySelectorAll('.box');
+const restart = document.querySelector('.restart');
+
+const Player = (symbol) => {
+    return { symbol };
+};
+
+function playTurn(element, player) {
+    if (!element.innerText) {
+        element.innerText = player.symbol;
+    }
+}
+
+function gameOver(player) {
+    if (
+        boxes[0].innerText == player.symbol && 
+        ((boxes[3].innerText == player.symbol && 
+        boxes[6].innerText == player.symbol) || 
+        (boxes[4].innerText == player.symbol && 
+        boxes[8].innerText == player.symbol))
+    ) {
+        return true;
+    } else if (
+        boxes[1].innerText == player.symbol && 
+        (boxes[4].innerText == player.symbol && 
+        boxes[7].innerText == player.symbol)
+    ) {
+        return true;
+    } else if (
+        boxes[2].innerText == player.symbol && 
+        ((boxes[5].innerText == player.symbol && 
+        boxes[8].innerText == player.symbol) || 
+        (boxes[4].innerText == player.symbol && 
+        boxes[6].innerText == player.symbol))
+    ) {
+        return true;
+    } else if (
+        boxes[3].innerText == player.symbol && 
+        (boxes[4].innerText == player.symbol && 
+        boxes[5].innerText == player.symbol)
+    ) {
+        return true;
+    }
+    return false;
+}
+
+function botRandomTurn(player) {
+    console.log("my turn!");
+    let attempt = 1 + Math.floor(Math.random() * 8);
+    while (boxes[attempt].innerText) {
+        attempt = 1 + Math.floor(Math.random() * 8);
+    }
+    playTurn(boxes[attempt], player)
+}
+
+let player1 = Player('X');
+let player2 = Player('O')
 
 boxes.forEach((element) => {
     element.addEventListener('click', () => {
-        element.innerText = 'W';
+        playTurn(element, player1);
+        if (!gameOver(player1)) {
+            botRandomTurn(player2);
+            gameOver(player2);
+        }
     });
 });
+
+restart.addEventListener('click', () => {
+    for (let box of boxes) {
+        box.innerText = '';
+    }
+})
